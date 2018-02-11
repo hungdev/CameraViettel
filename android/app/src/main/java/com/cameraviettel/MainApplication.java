507a52bdeac578e98e7ce44304c85b62c126a2d3
hungdev <hungns126@gmail.com>
2018-02-11 14:00:25 +0700

    add some lib

--- android/app/src/main/java/com/cameraviettel/MainApplication.java
+++ android/app/src/main/java/com/cameraviettel/MainApplication.java
@@ -3,6 +3,7 @@ package com.cameraviettel;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new RCTCameraPackage(),
             new RNGoogleSigninPackage()
       );
