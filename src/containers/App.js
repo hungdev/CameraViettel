1121924bec93e1a272b66cc3ee83f25ef0975b7a
hungdev <hungns126@gmail.com>
2018-02-24 01:11:13 +0700

    add redux persist

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,43 @@
-
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, AsyncStorage } from 'react-native';
 //Redux
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import allReducers from '../reducers';
 // import PinCodeScreen from './containers/PincodeScreen'
-import Camera from './CameraScreen'
-import Google from './GoogleSignin'
+// import Camera from './CameraScreen'
+// import Google from './GoogleSignin'
 //Redux saga
 import createSagaMiddleware from 'redux-saga';
-import rootSaga from '../sagas/rootSaga'; 
+import rootSaga from '../sagas/rootSaga';
 import AppNavigation from '../navigation/AppNavigation'
+import { REHYDRATE, PURGE, persistCombineReducers, persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // or whatever storage you are using
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 const sagaMiddleware = createSagaMiddleware();
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-let store = createStore(allReducers, applyMiddleware(sagaMiddleware));
+// let reducer = persistCombineReducers(config, allReducers)
+const persistedReducer = persistReducer(persistConfig, allReducers)
 
+let store = createStore(persistedReducer, undefined, compose(
+  applyMiddleware(sagaMiddleware),
+  // autoRehydrate()
+));
+let persistor = persistStore(store)
 export default class App extends Component<{}> {
   render() {
     return (
       <Provider store={store}>
-        {/* <Google /> */}
-        {/* <Camera /> */}
-        <AppNavigation />
-    </Provider>
+        <PersistGate loading={null} persistor={persistor}>
+          <AppNavigation />
+        </PersistGate>
+      </Provider>
     );
   }
 }
