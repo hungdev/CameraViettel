288db9776833db7089608396401f8ab7aa7f415c
hungdev <hungns126@gmail.com>
2018-02-25 11:57:32 +0700

    fix style modal button

--- src/containers/styles/GoogleSigninStyle.js
+++ src/containers/styles/GoogleSigninStyle.js
@@ -4,7 +4,7 @@ const { width, height } = Dimensions.get('window')
 export default StyleSheet.create({
   container: {
     // marginTop: 60,
-    // flex: 1,
+    flex: 1
     // justifyContent: 'center',
     // alignItems: 'center'
   },
