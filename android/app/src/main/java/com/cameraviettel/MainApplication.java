9920fe06645683c432df4e2c6f9b9f43848b8e73
hungdev <hungns126@gmail.com>
2018-03-07 01:01:56 +0700

    add vector icon

--- android/app/src/main/java/com/cameraviettel/MainApplication.java
+++ android/app/src/main/java/com/cameraviettel/MainApplication.java
@@ -3,6 +3,7 @@ package com.cameraviettel;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactlibrary.RNThumbnailPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNThumbnailPackage(),
             new RNFetchBlobPackage(),
             new RCTCameraPackage(),
